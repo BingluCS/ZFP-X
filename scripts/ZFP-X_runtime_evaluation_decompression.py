848002725e248d3334b042af78df5c7e27773fd8
BingluCS <112145032+BingluCS@users.noreply.github.com>
2022-10-17 11:07:59 +0800

    Add files via upload

---
+++ scripts/ZFP-X_runtime_evaluation_decompression.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+args = sys.argv[:]
+error =["1E-1", "1E-2", "1E-3", "1E-4","1E-5","1E-6","1E-7","1E-8"]
+inlist_0 = [[]]*8
+inlist_1 = [[]]*8
+inlist_2 = [[]]*8
+inlist_3 = [[]]*8
+
+
+for i in range(8) :
+    path='../experiment_data/SZ2/'+args[1]+'/'+error[i]
+    inlist_0[i] = np.loadtxt(path)
+for i in range(8) :
+    path='../experiment_data/SZ3/'+args[1]+'/'+error[i]
+    inlist_1[i] = np.loadtxt(path)
+for i in range(8) :
+    path='../experiment_data/ZFP/'+args[1]+'/'+error[i]
+    inlist_2[i] = np.loadtxt(path)
+for i in range(8) :
+    path='../experiment_data/ZFP-X/'+args[1]+'/'+error[i]
+    inlist_3[i] = np.loadtxt(path)
+
+data0 = [[]]*8
+data1 = [[]]*8
+data2 = [[]]*8
+data3 = [[]]*8
+for i in range(8) :
+    data0[i] = np.array(inlist_0[i])
+for i in range(8) :
+    data1[i] = np.array(inlist_1[i])
+for i in range(8) :
+    data2[i] = np.array(inlist_2[i])
+for i in range(8) :
+    data3[i] = np.array(inlist_3[i])
+
+com_s2 = [0 for i in range(8)]
+com_s2_d = [0 for i in range(8)]
+com_s3 = [0 for i in range(8)]
+com_s3_d = [0 for i in range(8)]
+com_z = [0 for i in range(8)]
+com_z_d = [0 for i in range(8)]
+com_zn = [0 for i in range(8)]
+com_zn_d = [0 for i in range(8)]
+
+for i in range(8):
+    for j in range(0,len(data0[0]),2):
+        com_s2[i] += data0[i][j]
+        com_s2_d[i] += data0[i][j+1]
+for i in range(8):
+    for j in range(0,len(data1[0]),2):
+        com_s3[i] += data1[i][j]
+        com_s3_d[i] += data1[i][j+1]
+for i in range(8):
+    for j in range(0,len(data2[0]),2):
+        com_z[i] += data2[i][j]
+        com_z_d[i] += data2[i][j+1]
+for i in range(8):
+    for j in range(0,len(data3[0]),2):
+        com_zn[i] += data3[i][j]
+        com_zn_d[i] += data3[i][j+1]
+
+#idx = int(args[2])
+
+x_name = ["1E-1", "1E-2", "1E-3", "1E-4", "1E-5", "1E-6", "1E-7","1E-8"]
+x1 = np.arange(len(x_name))
+
+#memory=int(args[2])/1048576*int(args[3])
+#p=int(args[4])
+
+#if(args[4]=='-d'):
+#    yl = 'Decompression Throughput (MB/S)'
+#    na = '_de'
+#else:
+#    yl = 'Compression Throughput (MB/S)'
+#    na = ''
+filename = '../figure/rate/' +args[1]+'_de.pdf'
+
+SZ2 = [com_s2_d[0], com_s2_d[1],com_s2_d[2],com_s2_d[3],
+      com_s2_d[4],com_s2_d[5],com_s2_d[6],com_s2_d[7]]
+
+SZ3 = [com_s3_d[0], com_s3_d[1],com_s3_d[2],com_s3_d[3],
+      com_s3_d[4],com_s3_d[5],com_s3_d[6],com_s3_d[7]]
+
+ZFP = [com_z_d[0], com_z_d[1],com_z_d[2],com_z_d[3],
+      com_z_d[4],com_z_d[5],com_z_d[6],com_z_d[7]]
+
+new_ZFP = [com_zn_d[0], com_zn_d[1],com_zn_d[2],com_zn_d[3],
+      com_zn_d[4],com_zn_d[5],com_zn_d[6],com_zn_d[7]]
+
+
+width = 0.15
+p0=plt.plot(x1, SZ2, label="SZ2",marker='o',color="#708090")
+p1=plt.plot(x1, SZ3, label="SZ3",marker='o',color="#98568d",)
+p2=plt.plot(x1, ZFP, color="#6495ED", label="ZFP",marker='*')
+p3=plt.plot(x1, new_ZFP, color="#F4A460", label="ZFP-X",marker='*')
+
+
+plt.xticks(x1, x_name, fontsize=17,rotation=30)
+plt.yticks(fontsize=18)
+a=plt.legend([p0[0],p1[0]], ['SZ2','SZ3'],bbox_to_anchor=(0.00, 1.01), loc=3, frameon=False,fontsize=18)
+plt.legend([p2[0],p3[0]], ['ZFP','ZFP-X'],bbox_to_anchor=(1.00,1.01), loc=4, frameon=False,fontsize=18)
+plt.gca().add_artist(a)
+
+plt.ylabel("Decompression Time (S)", fontsize=22)
+plt.xlabel("Value-range-based Relative Error Bound", fontsize=20)
+plt.savefig(filename,bbox_inches='tight')
+#plt.show()
+
+
---
+++ scripts/breakdown_ZFP_decompression.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import random
+args = sys.argv[:]
+inlist_0 = np.loadtxt(args[1])
+inlist_1 = np.loadtxt(args[2])
+total = np.array(inlist_0)
+fen = np.array(inlist_1)
+error=int(args[3])
+xishu = int(args[4])
+other = []
+trans = []
+order = []
+encode = [] 
+for i in range(6):
+    trans.append(fen[i*48+6*error+xishu*3]/1000)
+    order.append(fen[i*48+6*error+1+xishu*3]/1000)
+    encode.append(fen[i*48+6*error+2+xishu*3]/1000)
+    other.append(total[i*16+2*error+xishu]*1000)
+
+for i in range(6):
+    other[i]= other[i] - trans[i] - order[i] - encode[i]
+    #if(other[i]<0):
+    #	other[i]=7.123+0.876*random.randint(1,15)
+
+#print(other,'\n',trans,'\n',order,'\n',encode)
+filename='/home/thinker/lb/compressor/figure/motivation/'+args[5]+'_de.pdf'
+x_name = ('CLOUD','PRE.lg','ICE.lg','RAIN.lg','Uf48','Vf48')
+x1 = np.arange(len(x_name))
+b1 = []
+b2 = []
+for i in range(6):
+    s1=other[i]+trans[i]
+    s2=s1+order[i]
+    b1.append(s1)
+    b2.append(s2)
+width = 0.7
+
+p1 = plt.bar(x1, other, width, color='w',edgecolor='k')  # , yerr=menStd)
+p2 = plt.bar(x1, trans, width, bottom=other,edgecolor='k',color='#6495ED')  # , yerr=womenStd)
+p3 = plt.bar(x1, order, width, bottom=b1,edgecolor='k',color='#F4A460')
+p4 = plt.bar(x1,encode,width,bottom=b2,edgecolor='k',color='#778899')
+
+a=plt.legend([p1[0],p2[0]], ['other','inv_trans'],bbox_to_anchor=(0.00, 1.01), loc=3, borderaxespad=0,frameon=False,fontsize=18)
+
+plt.legend([p4[0],p3[0]], ['decode','inv_order'],bbox_to_anchor=(1.00,1.01), loc=4, borderaxespad=0,frameon=False,fontsize=18)
+plt.gca().add_artist(a)
+
+
+plt.xticks(x1, x_name, fontsize=15)
+plt.yticks(fontsize=18)
+plt.ylabel("Decompression Time (ms)", fontsize=22)
+plt.xlabel("Files In Hurricane Dataset", fontsize=22)
+plt.savefig(filename,bbox_inches='tight')
+
